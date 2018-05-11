50c90a484bf01ff278f64badc57273535fc50b55
Никита <nikitakhristov23@gmail.com>
2018-05-11 15:37:35 +0300

    Адаптивная графика

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,7 +1,7 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove("main-nav__toggle--nojs");
+navToggle.classList.remove("main-nav__toggle--js");
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains("main-nav--closed")) {
@@ -12,3 +12,17 @@ navToggle.addEventListener('click', function() {
       navMain.classList.remove("main-nav--opened");
     }
   });
+
+var linkChooseSize = document.querySelector(".product-card__order-button");
+var popup = document.querySelector(".modal-window");
+var overlay = document.querySelector(".modal-window__overlay");
+
+linkChooseSize.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-window--closed");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-window--closed");
+});
