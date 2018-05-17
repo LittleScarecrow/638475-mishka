bb8dae03d15ee5b0f8261415f0a5f82951494984
Никита <nikitakhristov23@gmail.com>
2018-05-17 13:27:52 +0300

    Векторная графика и оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,16 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var svgstore = require("gulp-svgstore");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -14,6 +21,9 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
@@ -29,4 +39,50 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("build/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+})
+
+gulp.task("images", function () {
+  return gulp.src("build/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/css/**",
+    "source/**/*.html"
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("build/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
 });
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -16,10 +16,21 @@ navToggle.addEventListener('click', function() {
 var linkChooseSize = document.querySelector(".product-card__order-button");
 var popup = document.querySelector(".modal-window");
 var overlay = document.querySelector(".modal-window__overlay");
+var linkOrder = document.querySelector(".catalog-items__icon-cart");
 
 linkChooseSize.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-window--closed");
+  popup.classList.remove("modal-window--closed");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-window--closed");
+});
+
+linkOrder.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-window--closed");
 });
 
 overlay.addEventListener("click", function (evt) {
