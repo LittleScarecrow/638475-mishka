53ae51a306274e2bea5cb2382d6cccfe278fde18
Никита <nikitakhristov23@gmail.com>
2018-05-23 15:06:46 +0300

    Исправления

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,18 +2,11 @@ var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
 navToggle.classList.remove("main-nav__toggle--js");
-navMain.classList.remove("main-nav--opened");
-navMain.classList.add("main-nav--closed");
+navMain.classList.toggle("main-nav--closed");
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-    } else {
-      navMain.classList.add("main-nav--closed");
-      navMain.classList.remove("main-nav--opened");
-    }
-  });
+    navMain.classList.toggle("main-nav--closed");
+})
 
 var linkChooseSize = document.querySelector(".product-card__order-button");
 var popup = document.querySelector(".modal-window");
